44588eb147b88dafc9e764adeb468579a1996f1e
gudrunliljaz <128697649+gudrunliljaz@users.noreply.github.com>
2025-04-28 15:51:37 +0000

    Update verkx_code.py

--- verkx_code.py
+++ verkx_code.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 PAST_FILE = "data/GÖGN_VERKX.xlsx"
-FUTURE_FILE = "data/Framtíðarspá.xlsx"
+FUTURE_FILE = "data/Framtidarspa.xlsx"
 
 def load_excel(file_path, sheet_name):
     df = pd.read_excel(file_path, sheet_name=sheet_name, engine="openpyxl")
