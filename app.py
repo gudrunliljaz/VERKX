39ffd7a9725471f28023963579a63bc76c523347
gudrunliljaz <128697649+gudrunliljaz@users.noreply.github.com>
2025-04-28 16:12:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if st.button("Keyra spá"):
             tabs = st.tabs(["Niðurstöður", "Vista niðurstöður"])
 
             with tabs[0]:
-                st.subheader("Niðurstöður")
+                st.subheader("Einingar")
                 st.dataframe(df.set_index("Ár").style.format("{:.2f}"))
 
                 st.subheader("Monte Carlo dreifing")
