b725fd484095ff1d1c78e1af5e296c28f1f7f5e4
gudrunliljaz <128697649+gudrunliljaz@users.noreply.github.com>
2025-05-01 13:51:18 +0000

    Update verkx_code.py

--- verkx_code.py
+++ verkx_code.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 PAST_FILE = "data/GÖGN_VERKX.xlsx"
-FUTURE_FILE = "data/Framtíðarspá.xlsx"
+FUTURE_FILE = "data/Framtidarspa.xlsx"
 
 # Kostnaðar- og tekjuforsendur
 PRICE_PER_SQM = 467_308
