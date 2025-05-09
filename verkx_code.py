dfaad253a3f5736279c874ea6932a0dbb71cc992
gudrunliljaz <128697649+gudrunliljaz@users.noreply.github.com>
2025-05-09 11:27:48 +0000

    Update verkx_code.py

--- verkx_code.py
+++ verkx_code.py
@@ -150,9 +150,9 @@ def main_forecast_logic_from_excel(past_file, future_file, share_file, profit_ma
         return None
 
     df_all = pd.concat(all_rows)
-    summary = df_all.groupby("Ár")["Meðaltal"].sum().reset_index()
-    summary["Fermetrar"] = summary["Meðaltal"].round(0).astype(int) * UNIT_SIZE_SQM
-    summary["Kostnaðarverð eininga"] = summary["Fermetrar"] * (0.19*269700 + 0.80*290000 + 0.01*304500 + 0.001*330000)
+    summary = df_all.groupby("ár")["meðaltal"].sum().reset_index()
+    summary["Fermetrar"] = summary["meðaltal"].round(0).astype(int) * UNIT_SIZE_SQM
+    summary["Kostnaðarverð eininga"] = summary["fermetrar"] * (0.19*269700 + 0.80*290000 + 0.01*304500 + 0.001*330000)
     summary["Flutningskostnaður"] = summary["fermetrar"] * 74000
     summary["Afhending innanlands"] = summary["fermetrar"] * 80 * 8
     summary["Fastur kostnaður"] = FIXED_COST_PER_YEAR
